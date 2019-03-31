998f488898253f20097f55e8ae651d7109b956ac
kkoomin <minha.koo.9@gmail.com>
2019-03-31 18:20:07 +0100

    finish

---
+++ App.js
@@ -0,0 +1,113 @@
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import Pokemon from "./Pokemon";
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+    isPokemonChosen: false,
+    pokemons: {},
+    selectedPokemon: undefined
+  };
+
+  componentDidMount = () => {
+    return fetch("http://localhost:3000/pokemon")
+      .then(res => res.json())
+      .then(data => this.setState({ pokemons: data }));
+  };
+
+  _handleBtnPress = () => {
+    console.log(this.state.pokemons.length);
+    const randomNo = Math.floor(Math.random() * 150);
+    this.setState({
+      isPokemonChosen: true,
+      selectedPokemon: this.state.pokemons[randomNo]
+    });
+  };
+
+  _backBtnPress = () => {
+    this.setState({ isPokemonChosen: false });
+  };
+
+  _choosePokemon = () => {
+    const randomNo = Math.floor(Math.random() * 150);
+    this.setState({ selectedPokemon: this.state.pokemons[randomNo] });
+  };
+
+  render() {
+    const { isPokemonChosen, selectedPokemon, pokemons } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        {isPokemonChosen ? (
+          <Pokemon
+            choosePokemon={this._choosePokemon}
+            selectedPokemon={selectedPokemon}
+            backBtnPress={this._backBtnPress}
+          />
+        ) : (
+          <View style={styles.main}>
+            <Text style={styles.title}>Dr.Oak's Lab</Text>
+            <Image
+              source={require("./assets/Professor_Oak_XY.png")}
+              style={styles.welcomeImage}
+            />
+            <Text style={styles.description}>
+              "Hey! Wait! Don't go out! It's unsafe! You need your own Pokémon
+              for your protection. Here, come with me!"
+            </Text>
+            <TouchableOpacity onPress={this._handleBtnPress}>
+              <View style={styles.buttonContainer}>
+                <Text style={styles.button}>Choose Pokemon</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#dee8eb",
+    alignItems: "center"
+  },
+  main: {
+    alignItems: "center"
+  },
+  pokemon: {},
+  title: {
+    color: "#3e3533",
+    fontSize: 50,
+    marginTop: 100,
+    marginBottom: 20
+  },
+  description: {
+    color: "#726b67",
+    fontSize: 18,
+    margin: 30,
+    marginBottom: 30,
+    textAlign: "center"
+  },
+  button: {
+    color: "#64b3d3",
+    fontSize: 20
+  },
+  buttonContainer: {
+    borderColor: "#64b3d3",
+    borderRadius: 10,
+    borderWidth: 2,
+    padding: 15,
+    paddingLeft: 50,
+    paddingRight: 50
+  }
+});
---
+++ Pokemon.js
@@ -0,0 +1,129 @@
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image
+} from "react-native";
+
+export default class Pokemon extends React.Component {
+  state = {
+    confirmPokemon: false
+  };
+
+  _confirmPokemon = () => {
+    this.setState({ confirmPokemon: true });
+  };
+
+  render() {
+    const { choosePokemon, selectedPokemon, backBtnPress } = this.props;
+    const { confirmPokemon } = this.state;
+    return (
+      <View>
+        <View style={styles.pokemon}>
+          <Image
+            source={{ uri: selectedPokemon.sprites.front }}
+            style={{
+              width: 300,
+              height: 300,
+              marginTop: 50
+            }}
+          />
+          <Text style={styles.name}>
+            {selectedPokemon.name.charAt(0).toUpperCase() +
+              selectedPokemon.name.slice(1)}
+          </Text>
+          <Text style={styles.info}>
+            Type: {selectedPokemon.types.join(", ")}
+          </Text>
+        </View>
+        <View>
+          {confirmPokemon ? (
+            <>
+              <Text style={styles.confirmed}>
+                {selectedPokemon.name.charAt(0).toUpperCase() +
+                  selectedPokemon.name.slice(1)}
+                , You're Mine!
+              </Text>
+              <TouchableOpacity onPress={backBtnPress}>
+                <View style={styles.buttonContainer2}>
+                  <Text style={styles.button2}>Back to Dr.Oak</Text>
+                </View>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <>
+              <TouchableOpacity onPress={choosePokemon}>
+                <View style={styles.buttonContainer1}>
+                  <Text style={styles.button1}>Choose Again</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this._confirmPokemon}>
+                <View style={styles.buttonContainer2}>
+                  <Text style={styles.button2}>I choose you!</Text>
+                </View>
+              </TouchableOpacity>
+            </>
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pokemon: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 100
+  },
+  name: {
+    color: "#3e3533",
+    fontSize: 30,
+    margin: 30,
+    marginBottom: 30,
+    textAlign: "center"
+  },
+  info: {
+    color: "#726b67",
+    fontSize: 20,
+    marginBottom: 30,
+    textAlign: "center"
+  },
+  button1: {
+    color: "#999591",
+    fontSize: 20
+  },
+  button2: {
+    color: "#64b3d3",
+    fontSize: 20
+  },
+  buttonContainer1: {
+    alignItems: "center",
+    borderColor: "#999591",
+    borderRadius: 10,
+    borderWidth: 2,
+    padding: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginTop: 20
+  },
+  buttonContainer2: {
+    alignItems: "center",
+    borderColor: "#64b3d3",
+    borderRadius: 10,
+    borderWidth: 2,
+    padding: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginTop: 20
+  },
+  confirmed: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#3e3533",
+    marginTop: 20
+  }
+});
